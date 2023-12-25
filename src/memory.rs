9ef1b46ff98294a1c43633031c640222202b0abd
jprahman <jason.p.rahman@gmail.com>
2023-12-24 15:55:03 -1000

    Simple load and store implementations

--- src/memory.rs
+++ src/memory.rs
@@ -12,13 +12,52 @@ impl Memory {
         }
     }
 
-    pub fn load(&self, addr: u64) -> u64 {
+    pub fn lw(&self, addr: u64) -> u32 {
         let addr = addr as usize;
-        u64::from_le_bytes(self.physical_memory[addr..addr + 8].try_into().unwrap())
+        u32::from_le_bytes(self.physical_memory[addr..addr + 8].try_into().unwrap())
+    }
+
+    pub fn lh(&self, addr: u64) -> i32 {
+        let addr = addr as usize;
+        let hword = u16::from_le_bytes(self.physical_memory[addr..addr + 2].try_into().unwrap());
+        hword as i32
+    }
+
+    pub fn lhu(&self, addr: u64) -> u32 {
+        let addr = addr as usize;
+        let hword = u16::from_le_bytes(self.physical_memory[addr..addr + 2].try_into().unwrap());
+        hword as u32
+    }
+
+    pub fn lb(&self, addr: u64) -> i32 {
+        let addr = addr as usize;
+        let bword = self.physical_memory[addr];
+        bword as i32
+    }
+
+    pub fn lbu(&self, addr: u64) -> u32 {
+        let addr = addr as usize;
+        let bword = self.physical_memory[addr];
+        bword as u32
+    }
+
+    pub fn sw(&mut self, addr: u64, value: u32) {
+        let addr = addr as usize;
+        self.physical_memory[addr..addr + 4].copy_from_slice(&value.to_le_bytes());
+    }
+
+    pub fn sh(&mut self, addr: u64, value: u16) {
+        let addr = addr as usize;
+        self.physical_memory[addr..addr + 2].copy_from_slice(&value.to_le_bytes());
+    }
+
+    pub fn sb(&mut self, addr: u64, value: u8) {
+        let addr = addr as usize;
+        self.physical_memory[addr] = value;
     }
 
     pub fn load_inst(&self, addr: u64) -> u32 {
         let addr = addr as usize;
-        u32::from_le_bytes(self.physical_memory[addr..addr+4].try_into().unwrap())
+        u32::from_le_bytes(self.physical_memory[addr..addr + 4].try_into().unwrap())
     }
 }
